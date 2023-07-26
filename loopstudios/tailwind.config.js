b6e2af43371104286ff028289ce5ecbb9f1e7142
davidtheweb-dev <davidhermosilladuro@gmail.com>
2023-07-26 19:40:51 +0200

    Added loopstudios template

--- loopstudios/tailwind.config.js
+++ loopstudios/tailwind.config.js
@@ -1,7 +1,21 @@
 module.exports = {
   content: ['./src/*.html'],
   theme: {
-    extend: {},
+    screens: {
+      sm: '480px',
+      md: '768px',
+      lg: '976px',
+      xl: '1440px',
+    },
+    extend: {
+      fontFamily: {
+        sans: ['Josefin Sans', 'sans-serif'],
+        alata: ['Alata'],
+      },
+      letterSpacing: {
+        widest: '.3em',
+      },
+    },
   },
   plugins: [],
 };
